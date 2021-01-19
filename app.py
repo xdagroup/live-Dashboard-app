7392c1cec01b82637d084f63021ac357d41c0e9c
XDA Developers <75879427+xdagroup@users.noreply.github.com>
2021-01-19 15:46:06 +0530

    adding files

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from pusher import Pusher
+
+app = Flask(__name__)
+pusher = Pusher(app_id=u'1140291', key=u'03780cf2c4d7d92ce9d9', secret=u'ba9443f60e05f218184f', cluster=u'ap2')
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
+
+@app.route('/orders', methods=['POST'])
+def order():
+	data = request.form
+	pusher.trigger(u'order', u'place', {
+		u'units': data['units']
+	})
+	return "units logged"
+
+@app.route('/message', methods=['POST'])
+def message():
+	data = request.form
+	pusher.trigger(u'message', u'send', {
+		u'name': data['name'],
+		u'message': data['message']
+	})
+	return "message sent"
+
+@app.route('/customer', methods=['POST'])
+def customer():
+	data = request.form
+	pusher.trigger(u'customer', u'add', {
+		u'name': data['name'],
+		u'position': data['position'],
+		u'office': data['office'],
+		u'age': data['age'],
+		u'salary': data['salary'],
+	})
+	return "customer added"
+
+if __name__ == '__main__':
+	app.run(debug=True)
